ca4f059b6a261d67e44b82f0c27e9fed3afb2b1d
Dawid Pogorzelski <dawid@pogorzelski.dev>
2025-08-31 23:35:41 +0200

    wip

--- build-aux/workspace_status.ps1
+++ build-aux/workspace_status.ps1
@@ -1,7 +1,5 @@
-#!/usr/bin/env pwsh
-
 try {
-    $gitDescribe = & git --no-pager describe --always --dirty 2>$null
+    $gitDescribe = & git --no-pager describe --always --dirty
     if ($LASTEXITCODE -eq 0) {
         Write-Output "STABLE_BUILD_GIT_DESCRIBE $gitDescribe"
     } else {
